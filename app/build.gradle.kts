d824a06f4bff6c2a3d5ea1a6f81a5d2c0d2a5f51
Dimitar Todorov <d.todorov.vn@gmail.com>
2025-01-08 16:59:15 +0200

    Fix min sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.dstod.weatherappudemy"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dstod.weatherappudemy"
